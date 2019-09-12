d3cb12f660c2d42e665300530c40061a88bfe30e
Breno Thomaz Savanini <breno.savanini@gmail.com>
2019-09-12 12:39:45 -0300

    Update main.c

--- InsertionSort/main.c
+++ InsertionSort/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main(){
     int values[] = {4,5,2,3,7,8,1,9,0,6};
     int n = sizeof(values)/sizeof(values[0]);
     insertionSort(values, n);
@@ -12,8 +11,7 @@ int main()
     return 0;
 }
 
-void insertionSort(int arr[],int n)
-{
+void insertionSort(int arr[],int n){
     for(int i = 0;i < n;i++){
         int j = i-1;
         int val = arr[i];
